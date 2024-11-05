7a5fa234f00dc3012e7222af6621d0b5aad2bd59
Zeinab <zeinabansarian6280@gmail.com>
2024-11-05 12:05:25 +0330

    complete home page

---
+++ js/home-mobile.js
@@ -0,0 +1,59 @@
+
+
+
+const slider1 = new Swiper('.slider1', {
+    disableOnInteraction:false,
+    loop: true,
+    pauseOnMouseEnter:false,
+    effect: 'fade',
+  fadeEffect: {
+    crossFade: false
+  },
+  speed : 1000,
+  autoplay: {
+    delay: 3000,
+  },
+  });
+
+
+  gsap.registerPlugin(ScrollTrigger);
+
+let parallaxLeft = gsap.timeline();
+parallaxLeft.to('.topS' , {
+    x : "20vw"
+})
+ScrollTrigger.create({
+    animation:parallaxLeft , 
+    trigger : ".scrollContainer",
+    start:"top 20%",
+    end:"bottom -200vw",
+    scrub : 1,
+    
+    
+  })
+  let parallaxRight = gsap.timeline();
+  parallaxRight.to('.btmS' , {
+    x : "-20vw"
+  })
+  ScrollTrigger.create({
+    animation:parallaxRight , 
+    trigger : ".scrollContainer",
+    start:"top 20%",
+    end:"bottom -200vw",
+    scrub : 1,
+    
+
+})
+
+
+gsap.to(".gallery .section1",{
+  scrollTrigger:{
+     trigger:".gallery",
+     start:"top 10%",
+     end:"bottom bottom",
+    //  onEnter: () => document.querySelector("header").classList.add("color"),
+     
+    //  onLeaveBack: () => document.querySelector("header").classList.remove("color")
+
+  }
+ })
